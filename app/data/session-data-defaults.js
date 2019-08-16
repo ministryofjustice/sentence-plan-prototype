d71feef7035f3afb3b61b9769079e098a6aa8970
Chris Armstrong <mail@chrisarmstrong.io>
2019-08-16 14:13:54 +0100

    Create a basic plan with one user step and playback on overview page is done.

--- app/data/session-data-defaults.js
+++ app/data/session-data-defaults.js
@@ -22,18 +22,30 @@ module.exports = {
   "offenders": [
     {
       "id": "1",
-      "name": "Gary Hart",
-      "crn": "X087946",
-      "noms": "AE1234BD",
+      "firstname": "Sam",
+      "lastname": "Curtis",
+      "crn": "S000001",
+      "noms": "A0000AB",
       "keyworker": "Sarah Smith",
       "summary": [
-        { title:"Main offence(s)", value: "Death by dangerous driving"},
-        { title:"Sentence length", value:"Life imprisonment, 25 years"},
-        { title:"Start date", value: "18 April 2019"},
-        { title:"Date ISP due", value:"20 July 2019"},
-        { title:"PAROM eligibility date", value: "9 October 2019"},
-        { title:"Expected HDC/release date", value:"28 November 2028"}
+        { title:"Main offence(s)", value: "Robbery"},
+        { title:"Sentence type", value:"Extended Determinate Sentence"},
+        { title:"Sentence length", value:"138 months"},
+        { title:"Start date", value: "27 February 2019"},
+        { title:"Date ISP due", value:"13 March 2019"},
+        { title:"PAROM eligibility date", value: "20 December 2023"},
+        { title:"Expected HDC/release date", value:"19 June 2026"}
+      ],
+      "needs": [
+        { type:"Accommodation", risk: true},
+        { type:"ETE", risk:true},
+        { type:"Finance", risk: true},
+        { type:"Relationships", risk:true},
+        { type:"Alcohol misuse" },
+        { type:"Emotional well-being" },
+        { type:"Thinking and behaviour" }
       ]
+          
     }
   ]
 
