9df0946e85c8eb5ffdffa638558a25ef0b98b148
Chris Armstrong <mail@chrisarmstrong.io>
2019-09-02 10:12:17 +0100

    Added autocomplete for interventions. Fixed a few bugs too.

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -23,3 +23,36 @@ $('#js-toggle').click(
       $('.js-hidden').hide();
   }
 );
+
+
+
+
+const topics = [
+  'A – Z',
+  'Motivation & Engagement (M&E) Standalone',
+  'Thinking Skills Programme (TSP)',
+  'Kaizen',
+  'Becoming New Me Plus (BNM+)',
+  'New Me Strengths (NMS)',
+  'Alcohol Related Violence (ARV)',
+  'Building Skills for Recovery (BSR)',
+  'Drink Impaired Drivers Programme (DIDP)',
+  'Building Better Relationships (BBR)',
+  'Resolve',
+  'Choices, Actions, Relationships, Emotions (CARE)',
+  'Horizon',
+  'iHorizon',
+  'Healthy Sex Programme (HSP)',
+  'Healthy Identity Intervention (HII)',
+  'Developing Dialogues (DD)',
+  'Identity Matters (IM)',
+  'New Me MOT',
+  'Living as New Me (LNM)'
+]
+
+accessibleAutocomplete({
+  element: document.querySelector('#intervention-type-container'),
+  id: 'intervention-type', // To match it to the existing <label>.
+  source: topics,
+  showAllValues: true
+})
