e21549a08c814c00d327c18b935cd11a8796b9da
Chris Armstrong <mail@chrisarmstrong.io>
2019-09-03 16:20:21 +0100

    Reorder steps during setup process and some content updates

--- app/data/session-data-defaults.js
+++ app/data/session-data-defaults.js
@@ -28,10 +28,10 @@ module.exports = {
       "noms": "A0000AB",
       "keyworker": "Sarah Smith",
       "summary": [
-        { title:"Main offence(s)", value: "Robbery"},
-        { title:"Sentence type", value:"Extended Determinate Sentence"},
-        { title:"Sentence length", value:"138 months"},
-        { title:"Start date", value: "27 February 2019"},
+        // { title:"Main offence(s)", value: "Robbery"},
+        // { title:"Sentence type", value:"Extended Determinate Sentence"},
+        // { title:"Sentence length", value:"138 months"},
+        // { title:"Start date", value: "27 February 2019"},
         { title:"Date ISP due", value:"13 March 2019"},
         { title:"PAROM eligibility date", value: "20 December 2023"},
         { title:"Expected HDC/release date", value:"19 June 2026"}
