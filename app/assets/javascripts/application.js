a9f208b0aefe782dd65be9d1596a80ed885ab21b
Chris Armstrong <mail@chrisarmstrong.io>
2019-09-30 14:34:31 +0100

    Fixed some inputs not playing back answers

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -52,17 +52,25 @@ const topics = [
 
 
 if ($('#goal1-step1-intervention-type-container').length > 0) {
+  element = document.querySelector('#goal1-step1-intervention-type-container')
+  id = 'goal1-step1-intervention-type' // To match it to the existing <label>.
+
   accessibleAutocomplete ({
-    element: document.querySelector('#goal1-step1-intervention-type-container'),
-    id: 'goal1-step1-intervention-type', // To match it to the existing <label>.
+    element: element,
+    defaultValue: element.getAttribute('data-default-value'),
+    id: id,
     name: 'goal1-step1-intervention-type',
     source: topics,
     showAllValues: true
   })
 } else if ($('#goal1-step2-intervention-type-container').length > 0) {
+  element = document.querySelector('#goal1-step2-intervention-type-container')
+  id = 'goal1-step2-intervention-type' // To match it to the existing <label>.
+
   accessibleAutocomplete ({
-    element: document.querySelector('#goal1-step2-intervention-type-container'),
-    id: 'goal1-step2-intervention-type', // To match it to the existing <label>.
+    element: element,
+    defaultValue: element.getAttribute('data-default-value'),
+    id: id,
     name: 'goal1-step2-intervention-type',
     source: topics,
     showAllValues: true
@@ -70,17 +78,25 @@ if ($('#goal1-step1-intervention-type-container').length > 0) {
 }
 
 if ($('#goal2-step1-intervention-type-container').length > 0) {
+  element = document.querySelector('#goal2-step1-intervention-type-container')
+  id = 'goal2-step1-intervention-type' // To match it to the existing <label>.
+
   accessibleAutocomplete ({
-    element: document.querySelector('#goal2-step1-intervention-type-container'),
-    id: 'goal2-step1-intervention-type', // To match it to the existing <label>.
+    element: element,
+    defaultValue: element.getAttribute('data-default-value'),
+    id: id,
     name: 'goal2-step1-intervention-type',
     source: topics,
     showAllValues: true
   })
 } else if ($('#goal2-step2-intervention-type-container').length > 0) {
+  element = document.querySelector('#goal2-step2-intervention-type-container')
+  id = 'goal2-step2-intervention-type' // To match it to the existing <label>.
+
   accessibleAutocomplete ({
-    element: document.querySelector('#goal2-step2-intervention-type-container'),
-    id: 'goal2-step2-intervention-type', // To match it to the existing <label>.
+    element: element,
+    defaultValue: element.getAttribute('data-default-value'),
+    id: id,
     name: 'goal2-step2-intervention-type',
     source: topics,
     showAllValues: true
